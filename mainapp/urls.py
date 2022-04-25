6a26f919fc76d15bca3ed5f6529af588cddce317
Joel-Fah <joelfah2003@gmail.com>
2022-04-25 21:17:37 +0100

    Another important commit

--- mainapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from .views import mainView
-
-# Creat your urls here.
-urlpatterns = [
-    path('', mainView, name='app'),
-]
--- mainapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def mainView(request):
-    template_name = 'index.html'
-    context = {}
-    return render(request, template_name, context)
